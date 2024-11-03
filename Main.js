e099119b09891a65d7178b8f59eb0ed4243ae1f6
Afif-Tamim <afif.tamim04@gmail.com>
2024-11-03 09:41:14 +0200

    button css
    - Designed a button for submission
    - fixed a bit of the spacing
    - made the question direct

--- Main.js
+++ Main.js
@@ -1,15 +1,24 @@
 const slideValue = document.querySelector("span")
 const inputSlider = document.querySelector("input")
+
 inputSlider.addEventListener("input", () => {
     const value = slideValue.style.left = ((value - inputSlider.min) / (inputSlider.max - inputSlider.min)) * 100 + "%";
     inputSlider.style.background = `linear-gradient(to right, #5cab7d ${value}%, #ddd ${value}%)`;
 });
+
 inputSlider.oninput = (() => {
     let value = inputSlider.value;
     slideValue.textContent = value;
     slideValue.style.left = ((value - inputSlider.min) / (inputSlider.max - inputSlider.min)) * 100 + "%";
     slideValue.classList.add("show");
 });
+
 inputSlider.onblur = (() => {
     slideValue.classList.remove("show")
+});
+
+const submit = document.querySelector("button");
+
+submit.addEventListener('click', function () {
+
 });
